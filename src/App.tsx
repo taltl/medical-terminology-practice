47e3575b9458f7b76d57d930385ff19fb8b60736
talalum <talalum@gmail.com>
2024-03-28 20:00:19 +0200

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useRef, useState } from 'react';
 import './App.css';
+import Select from "react-select";
 
 function App() {
+
+  const colors = [{label: 'green', value: 0}, {label: 'blue', value: 1}, {label: 'red', value: 2}, {label: 'yellow', value: 3}];
+  const ref = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    if(ref.current){
+      console.log("asd: ", ref.current);
+      
+      ref.current?.focus();
+    }
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{backgroundColor:'blue', width: '300px', height: '300px'}} ref={ref}>
+      <h2>שלום לכולם</h2>
+      <Select
+          className="basic-single"
+          classNamePrefix="select"
+          defaultValue={colors[2]}
+          // isDisabled={false}
+          // isLoading={isLoading}
+          // isClearable={true}
+          isRtl={true}
+          // isSearchable={isSearchable}
+          name="color"
+          options={colors}
+        />
     </div>
   );
 }
