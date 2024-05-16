499f964f2026b6be4a94da53eeb5211463c52368
talalum <talalum@gmail.com>
2024-05-17 01:56:52 +0300

    replace link to navigation

--- src/components/quiz/startQuiz.tsx
+++ src/components/quiz/startQuiz.tsx
@@ -1,8 +1,11 @@
 import { Link } from "react-router-dom";
 import classes from "./../quiz/startQuiz/startQuiz.module.scss";
 import globalClasses from "./../../global.module.scss";
+import { useNavigate } from "react-router-dom";
 
 const StartQuiz: React.FC = () => {
+  const navigate = useNavigate();
+
   return (
     <>
       <div className={globalClasses.back}>
@@ -10,21 +13,36 @@ const StartQuiz: React.FC = () => {
       </div>
       <div className={classes.startTrivia}>
         <h1>נושאים לטריוויה</h1>
-        <Link to={"/medical-terminology-practice/trivia/1"}>
-          <button className={classes.link}>מילות שורש</button>
-        </Link>
-        <Link to={"/medical-terminology-practice/trivia/2"}>
-          <button className={classes.link}>סופיות</button>
-        </Link>
-        <Link to={"/medical-terminology-practice/trivia/3"}>
-          <button className={classes.link}>תחיליות</button>
-        </Link>
-        <Link to={"/medical-terminology-practice/trivia/4"}>
-          <button className={classes.link}>מילות מיקום</button>
-        </Link>
-        <Link to={"/medical-terminology-practice/trivia/5"}>
-          <button className={classes.link}>תרופות</button>
-        </Link>
+        <button
+          onClick={() => navigate("/medical-terminology-practice/trivia/1")}
+          className={classes.link}
+        >
+          מילות שורש
+        </button>
+        <button
+          onClick={() => navigate("/medical-terminology-practice/trivia/2")}
+          className={classes.link}
+        >
+          סופיות
+        </button>
+        <button
+          onClick={() => navigate("/medical-terminology-practice/trivia/3")}
+          className={classes.link}
+        >
+          תחיליות
+        </button>
+        <button
+          onClick={() => navigate("/medical-terminology-practice/trivia/4")}
+          className={classes.link}
+        >
+          מילות מיקום
+        </button>
+        {/* <button
+            onClick={() => navigate("/medical-terminology-practice/trivia/5")}
+            className={classes.link}
+          >
+            תרופות
+          </button> */}
       </div>
     </>
   );
